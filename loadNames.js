81d2d38769ef98eb3344433732cc710ba1956763
varelycode <varelycode@users.noreply.github.com>
2018-05-18 14:29:37 -0700

    Add files via upload

--- loadNames.js
+++ loadNames.js
@@ -4,29 +4,32 @@ var http = require('http');
 var sizeOf = require('image-size');
 var sqlite3 = require("sqlite3").verbose();
 var fs = require("fs");
-
+var entries = 0;
 var dbFileName = "PhotoQ.db";
 var db = new sqlite3.Database(dbFileName);
-
+var location = '';
+var list = '';
 http.globalAgent.maxSockets = 1;
-var len = obj.photoURLs.length; // number of images in photoURLS
+var len = obj.photoURLs.length;
 var i = 0;
+var height = 0;
+var width = 0;
+var index;
 
 
-for(i; i < len; i ++){ // for number of images in array 
 
+for(i; i < len; i ++){
 var firstPhoto = obj.photoURLs[i];
 var options = url.parse(firstPhoto);
 
-// get image dimensions
+
 http.get(options, function (response) {
   var chunks = [];
   response.on('data', function (chunk) {
     chunks.push(chunk);
   }).on('end', function() {
     var buffer = Buffer.concat(chunks);
-    console.log(sizeOf(buffer).height); // height usage: sizeOf(buffer).width/height
-// need to insert each image into the DB
+	saveTable(i, dbFileName, sizeOf(buffer).height, sizeOf(buffer).width, location, list);
 
 });
 });
@@ -34,13 +37,15 @@ http.get(options, function (response) {
 
 }
 
+dumpDB();
+
+
 
 var cmdStr = 'INSERT INTO photoTags VALUES(_IDX, "_FILENAME", _WIDTH, _HEIGHT, "_LOC", "_LIST")';
 var cbCount = 0;
 
 
 function insertDataCallback(err) {
-
         if(err){
                 console.log("Error while saving data in DB: ", err);
         }
@@ -50,17 +55,22 @@ function insertDataCallback(err) {
 
 
 }
-// add to table
-function saveTable(index, name, dims, location, list){
 
+function saveTable(index, name, height, width, location, list){
         var cmd = cmdStr.replace("_IDX", index);
         cmd = cmd.replace("_FILENAME", name);
-        cmd = cmd.replace("_WIDTH", dims.width);
-        cmd = cmd.replace("_HEIGHT", dims.height);
+        cmd = cmd.replace("_WIDTH", width);
+        cmd = cmd.replace("_HEIGHT", height);
         cmd = cmd.replace("_LOC", location);
         cmd = cmd.replace("_LIST", list);
-
         console.log("        item", index, "complete!");
         db.run(cmd, insertDataCallback);
 
+}
+
+function dumpDB() {
+  db.all ( 'SELECT * FROM photoTags', dataCallback);
+      function dataCallback( err, data ) {
+		console.log(data) 
+      }
 }
